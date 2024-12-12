3927d442a454f929cfa534eafac504d7fa89a39e
Arthur Lanzilotti Farjanes <arthurlfarjanes@gmail.com>
2024-12-12 17:27:06 -0300

    Correção de ortografia

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <div className="alignNavbar">
           <h3>
             <span className="highlight">Discover</span> some
-            <span className="highlight">more</span> about our business
+            <span className="highlight"> more</span> about our business
           </h3>
           <div className="links">
             <a href="#about-us">About us</a>|<a href="#services">Services</a>
