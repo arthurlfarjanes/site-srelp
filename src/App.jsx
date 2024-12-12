674a3e572f112229caf4596d71a0f7f02bbf85f6
Arthur Lanzilotti Farjanes <arthurlfarjanes@gmail.com>
2024-12-12 09:40:34 -0300

    Ajustes para novo build

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@
 import "./App.css";
 
 // React
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MenuContextProvider } from "./context/MenuContext/MenuContext";
 
 // Pages
 import LandingPage from "./pages/LandingPage/LandingPage";
-import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
